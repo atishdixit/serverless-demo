d768c3cfbd2c4777e3c9c40d12d03c630e0047a2
atishdixit <atishdixit26@gmail.com>
2023-01-04 18:04:02 +0530

    Initial commit

---
+++ src/test/java/com/extellon/ServerlessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extellon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlessDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
